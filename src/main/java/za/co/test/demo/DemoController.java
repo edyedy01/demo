ed8aafef32aeb0b7006b5000754e4f643dfdc4dd
eduardo.gasser <eduardog@interfile.co.za>
2025-11-26 08:36:51 +0200

    add service innerds add controller innerds

--- src/main/java/za/co/test/demo/DemoController.java
+++ src/main/java/za/co/test/demo/DemoController.java
@@ -1,19 +1,24 @@
 package za.co.test.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+    private PersonService personService;
 
     @PostMapping("/test-post")
-    public ResponseEntity<PersonResponse> hello(@RequestBody PersonRequest personRequest) {
+    public ResponseEntity<PersonResponse> testPost(@RequestBody PersonRequest personRequest) {
+        log.debug("Entering testPost:  {}", personRequest);
 
+        PersonResponse personResponse = personService.createPerson(personRequest);
 
-        return ResponseEntity.ok(PersonResponse.builder().id(1).build());
+        return ResponseEntity.ok(personResponse);
     }
 }
--- src/main/java/za/co/test/demo/PersonServiceImpl.java
+++ src/main/java/za/co/test/demo/PersonServiceImpl.java
@@ -1,8 +1,25 @@
 package za.co.test.demo;
 
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class PersonServiceImpl implements PersonService {
+    PersonRepository personRepository;
     @Override
     public PersonResponse createPerson(PersonRequest personRequest) {
+        PersonModel personModel = new PersonModel();
+        personModel.setFirstName(personRequest.firstName());
+        personModel.setSurname(personRequest.surname());
+        personModel.setHeight(personRequest.height());
+        personModel.setWeight(personRequest.weight());
+        personModel.setCreatedDate(LocalDateTime.now());
+
+        personRepository.save(personModel);
         return null;
     }
 }
